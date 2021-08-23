351e64410ba7d47d720f72024a3f7fedea8e690d
Kyle D <contact@KyleDiggs.com>
2021-08-23 15:11:14 -0700

    Adding page routes

--- web/src/pagerouter/Pagerouter.js
+++ web/src/pagerouter/Pagerouter.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function Pagerouter() {
   return (
-    <div>
-
+    <React.Fragment>
+      <Switch>
+        <Route exact path="/"><Auth /></Route>
+        <Route exact path="/feed"><Feed /></Route>
+        <Route exact path="/profile"><Profile /></Route>
+        <Route exact path="/profile/:pid"><Profile /></Route>
+      </React.Fragment>
     </div>
   )
 }
