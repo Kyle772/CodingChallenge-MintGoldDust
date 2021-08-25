2d75c0ff10bb50d21ce8ab47c851a3ed12bd2680
Kyle D <contact@KyleDiggs.com>
2021-08-25 12:33:08 -0700

    Adding blank directories that are missing in checkout

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(www.kylediggs.com)
+domains=(test.kylediggs.com)
 rsa_key_size=4096
 data_path="./server/certbot"
 email="Contact@KyleDiggs.com" # Adding a valid address is strongly recommended
